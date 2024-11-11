288dd696cde79155fb3ddcb433a4d824ce855f7d
heshan_sandeepa <heshan.sandeepa@freshmilksoftware.com>
2024-11-11 21:23:16 +0530

    proceed

--- codelab-android-dagger-main/app/src/main/java/com/example/android/dagger/splash/SplashViewModel.kt
+++ codelab-android-dagger-main/app/src/main/java/com/example/android/dagger/splash/SplashViewModel.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.dagger.spalsh
+package com.example.android.dagger.splash
 
 import com.example.android.dagger.user.UserDataRepository
 import javax.inject.Inject
